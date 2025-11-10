c9d5151b28b439a3a8c2e3df5bcc5cb012314966
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2025-11-10 00:34:48 +0000

    WAP to input a number and check whether it is odd or even

---
+++ odd_even.cpp
@@ -0,0 +1,12 @@
+//WAP to input a number and check whether it is odd or even
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Number is even";
+    else
+    cout<<"Number is odd";
+}
