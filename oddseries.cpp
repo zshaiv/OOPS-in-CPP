0937f76a021e3334d8af295ecc3088a7c7011eb5
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2025-11-10 00:01:48 +0000

    WAP to print odd series of number between 1 and 20

---
+++ oddseries.cpp
@@ -0,0 +1,10 @@
+//WAP to print odd series of number between 1 and 20
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=1;i<=20;i++){
+        if(i%2!=0)
+        cout<<i<<" ";
+    }
+}
