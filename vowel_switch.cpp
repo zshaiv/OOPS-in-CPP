ae5e10d1c96528e3b9a2f7eecfce88d54a324c79
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2025-11-10 16:16:58 +0000

    WAP to check vowels using switch

---
+++ vowel_switch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    switch(ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            cout << ch << " is a vowel." << endl;
+            break;
+        default:
+            cout << ch << " is not a vowel (it's a consonant or special character)." << endl;
+    }
+
+    return 0;
+}
