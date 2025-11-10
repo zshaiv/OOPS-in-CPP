ae6ae2c4c8d36d956724b1ae2b4f9a957ba2f716
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2025-11-10 17:18:41 +0000

    WAP to input a string and reverse it and display it

--- 2D-Array/String/reverse.cpp
+++ 2D-Array/String/reverse.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 int main() {
-    char str[200];
-    int count = 0;
+    char str[100];
+    int length = 0;
 
     cout << "Enter a string: ";
-    cin.getline(str, 200);  // Read a full line (including spaces)
+    cin.getline(str, 100);  // Reads full line (including spaces)
 
-    // Count characters (excluding null terminator)
-    for (int i = 0; str[i] != '\0'; i++) {
-        count++;
+    // Find length of string
+    while (str[length] != '\0') {
+        length++;
     }
 
-    cout << "Total number of characters: " << count << endl;
+    // Display reversed string
+    cout << "Reversed string: ";
+    for (int i = length - 1; i >= 0; i--) {
+        cout << str[i];
+    }
 
+    cout << endl;
     return 0;
 }
-
