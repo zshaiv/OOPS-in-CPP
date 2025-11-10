6ea685003e97a07f4f86f7846d94dcf66ea2a2d3
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2025-11-10 17:19:16 +0000

    WAP to input a string and count the words

--- 2D-Array/String/count_word.cpp
+++ 2D-Array/String/count_word.cpp
@@ -1,1 +1,20 @@
 //WAP to input a string and count the words
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[200];
+    int count = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 200);  // Read a full line (including spaces)
+
+    // Count characters (excluding null terminator)
+    for (int i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+
+    cout << "Total number of characters: " << count << endl;
+
+    return 0;
+}
