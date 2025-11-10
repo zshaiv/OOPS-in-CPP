6f2b8583098a3fd5a63d9a54c29c78addb28dbe1
Kumar Shaiv Sah <161468027+zshaiv@users.noreply.github.com>
2025-11-10 00:37:37 +0000

    WAP to input two numbers and find the maximum

---
+++ max.cpp
@@ -0,0 +1,15 @@
+//WAP to input two numbers and find the maximum
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter first number: ";
+    cin>>a;
+    int b;
+    cout<<"Enter first number: ";
+    cin>>b;
+    if(a>b)
+    cout<<a<<" is greater";
+    else
+    cout<<b<<" is greater";
+}
